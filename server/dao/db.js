933b4918c6968a7f6ced44c2e1de74116d12ff26
Daark <daarkienightglare@gmail.com>
2026-05-31 21:06:04 +0200

    Added folder for DB

--- server/dao/db.js
+++ server/dao/db.js
@@ -1,5 +1,15 @@
+const fs = require("fs");
+const path = require("path");
 const { DatabaseSync } = require("node:sqlite");
 
-const db = new DatabaseSync("./data/dacer_db.db");
+const dataDir = path.join(__dirname, "../../data");
+
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const dbPath = path.join(dataDir, "dacer_db.db");
+
+const db = new DatabaseSync(dbPath);
 
 module.exports = db;
